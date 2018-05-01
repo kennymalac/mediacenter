e3d079b52300db5f26c72db47c12c11bbf7a2ca6
Kenneth Malac <kennethm777@gmail.com>
2018-05-01 10:55:51 -0700

    Removed old FB parody code, added title to ContentItem

--- app/media/ui/static/program/src/components/Feed.vue
+++ app/media/ui/static/program/src/components/Feed.vue
@@ -4,46 +4,7 @@
         <feed-image item="{}" />
         <feed-image item="{}" />
         <feed-image item="{}" />
-
-
-    <div class="user-post">
-      <div class="user-legend">
-        <div class="user-img">
-          <img :src="default_user_profile_pic" height="64" width="64"/>
-          <span class="username">
-            First name Surname
-          </span>
-        </div>
-      </div>
-      <div class="post-content pure-u-1-3">
-        Here is something I found funny online:
-      </div>
-      <div class="post-comment-thread pure-u-1-2">
-        Comments:
-        <div class="post-comment">
-          Personally I found this offensive
-        </div>
-        <div class="post-comment">
-          Lighten up, it's just a joke! This is seriously what people do online.
-        </div>
-        <div class="post-comment">
-          I can't believe it.
-        </div>
-      </div>
     </div>
-    <div class="user-post pure-u-1-2">
-      <div class="user-legend">
-        <div class="user-img">
-         <img :src="default_user_profile_pic" height="64" width="64"/>
-          <span class="username">
-            First name Surname
-          </span>
-        </div>
-      </div>
-      <div class="post-content">
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -61,15 +22,4 @@ export default {
 </script>
 
 <style scoped>
-.user-legend, .user-post, .post-comment {
-  border: 1px solid gray;
-  border-radius: 2px;
-  padding: 10px;
-  }
-  img {
-  display: inline;
-  }
-  .post-content, .post-comment-thread {
-  margin-top: 10px;
-  }
 </style>
--- app/media/ui/static/program/src/components/FeedItems/ContentItem.vue
+++ app/media/ui/static/program/src/components/FeedItems/ContentItem.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="content-item">
+        <slot name="title">
+            <div class="content-title">
+                Test by <a href="#" @click.native="userProfile" class="author">@datboi</a>
+            </div>
+        </slot>
         <slot name="embed" :slotProps="embedProps">
         </slot>
         <div class="actions">
@@ -27,22 +32,38 @@ export default {
     mixins: [FeedItem],
     props: {
         embedProps: [Object]
+    },
+    methods: {
+        userProfile(event) {
+            event.preventDefault()
+        }
     }
 }
 </script>
 
 <style lang="scss">
+$actions-height: 54px;
+$title-height: 24px;
+
 .content-item {
     display: inline-flex;
     flex-direction: column;
     border: 2px solid rgba(52, 73, 94,1.0);
     border-radius: 4px / 5px;
     width: 333px;
-    height: 240px;
+    height: 280px;
+
+    .content-title {
+        height: $title-height;
+        font-size: 1.4rem;
+        width: 100%;
+        color: rgb(52, 73, 94);
+    }
+
     .actions {
         display: flex;
         align-self: flex-end;
-        height: 54px;
+        height: $actions-height;
         width: 100%;
         .action {
             background-color: #dbe4ed;
@@ -71,7 +92,7 @@ export default {
         i { font-size: 10rem; }
         display: flex;
         width: 100%;
-        height: calc(100% - 54px);
+        height: calc(100% - #{$actions-height} - #{$title-height});
     }
 }
 </style>
